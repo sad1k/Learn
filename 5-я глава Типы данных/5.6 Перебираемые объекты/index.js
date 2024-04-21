32676724577ef4a441a9ae552253861e9580f5cc
sad1k <misha.kirillov.0990@gmail.com>
2024-04-21 16:20:45 +0300

    katana debt

---
+++ 5-я глава Типы данных/5.6 Перебираемые объекты/index.js
@@ -0,0 +1,38 @@
+// [Symbol.iterator]
+// Сложность 2/10
+
+// Написать итератор, чтобы выводил рандомные натуральные числа в диапазоне [0..500] и
+// итерировал пока не найдет заданное рандомное число (заранее вычисленное по этому же алгоритму
+// и имеющееся в свойстве объекта) и показано перед циклом. Алгоритм вынести в отдельную функцию,
+// чтобы можно было заюзать как в итераторе так и в объекте при инициализации.
+
+// Заюзать разные способы итерации по итератору.
+
+function randomNum() {
+  return Math.floor(Math.random() * 500 + 1);
+}
+
+function iterate(){
+    console.log(this.num);
+    return {
+      next() {
+        this.variant = randomNum();
+        console.log(this.variant);
+        if (this.num === this.variant) {
+          return { done: true };
+        } else {
+          return { done: false, value: this.variant };
+        }
+      },
+    };
+}
+
+let obj = {
+  num: randomNum(),
+  variant: -1,
+  [Symbol.iterator]: iterate
+};
+
+for(let elem of obj){
+
+}// бесконечный цикл :(
